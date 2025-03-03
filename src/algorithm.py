390323b3267db35a8fb5772a18ebefdff00c9ed1
AlissaStorm <Alissaalidib@hotmail.com>
2025-03-03 17:06:35 +0100

    made the encode method for the program

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,5 +1,34 @@
-def encode():
-    return None
+
+alphabet = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','æ','ø','å')
+
+def encode(input):
+
+    splitInput = list(str(input.lower()))
+    encodedBinary = ""
+    binary = []
+
+    for j in range(len(splitInput)):
+        binaryControlValue = 16
+        for i in range(len(alphabet)):
+
+            if alphabet[i] == splitInput[j]:
+                indexForCharacter = i+1
+                for k in range(5):
+                    if indexForCharacter >= binaryControlValue:
+                        indexForCharacter = indexForCharacter - binaryControlValue
+                        encodedBinary += "1"
+                    else:
+                        encodedBinary += "0"
+
+                    binaryControlValue = binaryControlValue/2
+
+                encodedBinary += " "
+            elif splitInput[j] == " ":
+                encodedBinary += ", "
+                break
+    return encodedBinary
+
+
 
 def decode():
     return None
--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from src.algorithm import encode, decode
 
-choice = int(input("Which one would i like to choose"))
 print("1. Encode")
 print("2. Decode")
+choice = int(input("Which one would you like to choose: "))
+
 
 if choice == 1:
     print("You have chosen encode")
-    encode()
+    chosenSentence = input("What would you like to encode to binary?: ")
+    encodedBinary = encode(chosenSentence)
+    print(encodedBinary)
+
 elif choice == 2:
     print("You have chosen decode")
     decode()
